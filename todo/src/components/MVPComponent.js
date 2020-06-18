4b2974b0775062e366c1d2d4c0b30d3320190380
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-06-17 21:03:38 -0400

    Thankful for YouTube

--- todo/src/components/MVPComponent.js
+++ todo/src/components/MVPComponent.js
@@ -1,26 +1,45 @@
-import React, {useReducer, useState} from 'react';
-import {reducer, initialState} from '../reducers/simpleReducer'
+import React, { useReducer, useState } from "react";
+import { reducer, initialState } from "../reducers/simpleReducer";
 
+const MVPComponent = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [text, setText] = useState();
 
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "ADD_TODO", text });
+          setText("");
+        }}
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </form>
 
+      <div>
+        {state.todos.map((todo) => (
+          <div
+            className="todo"
+            onClick={() => {
+              dispatch({ type: "TOGGLE_TODO", id: todo.id });
+            }}
+            style={{ textDecoration: todo.completed ? "line-through" : "" }}
+          >
+            {" "}
+            {todo.item}{" "}
+          </div>
+        ))}
+        {console.log(state.todos)}
+      </div>
 
-const MVPComponent = () => { 
-    
+    </div>
+  );
+};
 
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [text, setText] = useState();
-
-    return (
-        <div>
-            <h1>To Do List</h1>
-            <form onSubmit={e => { 
-                e.preventDefault();
-                dispatch({type: "add-todo", payload: text})
-            }}>
-                <input type='text' value={text} onChange={e => setText(e.target.value)} />
-            </form>
-        </div>
-    )
-}
-
-export default MVPComponent
+export default MVPComponent;
--- todo/src/reducers/simpleReducer.js
+++ todo/src/reducers/simpleReducer.js
@@ -1,24 +1,67 @@
 import React from 'react';
 
 export const initialState = {
+    todos: [{ 
   item: "Learn about reducers",
   completed: false,
   id: 3892987589,
+    }]
 };
 
+
+
 export const reducer = (state, action) => {
-  switch (action.type) {
-    case "add-todo":
-      return {
-        todos: [...state.todos, { item: action.text, completed: false}],
-      };
-    // case "complete-todo":
-    //     return { 
-    //         todos: state.todos.map((t, index) => {
-    //             index === action.index ? {...t, completed: !t.completed } : t
-    //         })
-    //     }
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+      case "ADD_TODO":
+          console.log(action)
+        return {
+           todos: [...state.todos, {item: action.text, completed:false, id: new Date()}]
+        }
+      case "TOGGLE_TODO": 
+          console.log(action)
+          
+          return  {
+              todos: state.todos.map(todo => todo.id === action.id ? {...todo, completed: !todo.completed} : todo)
+          }
+              
+          
+      default:
+        return state;
+    }
+  };
+  
+
+// export const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "ADD_TODO":
+//         console.log(action)
+//       return [
+//           ...state,
+//         {item: action.payload,
+//         completed: false,
+//         id: new Date(),}
+        
+//       ];
+//     case "TOGGLE_TODO":
+//         console.log(action)
+        
+//         return  [
+//                 state
+//         //   state.map(todo =>  
+//         //       todo.id === action.id ? !todo.completed : todo
+//         //   )
+            
+//         ]
+//     default:
+//       return state;
+//   }
+// };
+
+
+// ...state,
+        //     {item: action.id,
+        //     completed: !state.completed}
+        //     // todos: state.todos.map((t, index) => {
+        //     //     index === action.index ? {...t, completed: !t.completed } : t
+        //     // })
+        // ]
